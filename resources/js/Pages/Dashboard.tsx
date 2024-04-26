1cac63a15ef63fb69975fd604c2340c7015f44c3
Tomáš Reinert <reinerttomas@gmail.com>
2024-04-26 20:02:32 +0200

    install shadcn

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,6 +1,7 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 import { PageProps } from '@/types';
+import {Button} from "@/Components/ui/button";
 
 export default function Dashboard({ auth }: PageProps) {
     return (
@@ -11,11 +12,16 @@ export default function Dashboard({ auth }: PageProps) {
             <Head title="Dashboard" />
 
             <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
-                    </div>
+              <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                  <div className="p-6">You're logged in!</div>
                 </div>
+                <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mt-4">
+                  <div className="p-6">
+                    <Button>Click me</Button>
+                  </div>
+                </div>
+              </div>
             </div>
         </AuthenticatedLayout>
     );
